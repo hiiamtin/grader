93e2034331f41d62ee5af411ddf79d2939c59092
hiiamtin <tin5451@hotmail.com>
2020-08-25 13:36:07 +0700

    app controller

--- compro20s1/application/controllers/Welcome.php
+++ compro20s1/application/controllers/Welcome.php
@@ -1,25 +1,52 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+define('MY_CONTROLLER', pathinfo(__FILE__, PATHINFO_FILENAME));
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+	{
+		parent::__construct();
+		
+	}
+	
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('welcome_test');
+		//$this->load->view('head');
+		//$this->load->view('body_head');
+		//$this->load->view('body_nav');
+		//$this->load->view('body_main');
+		//$this->load->view('footer');
+		//$this->load->view('test_layout_webpage');
+		redirect('/Auth');
+		//$this->load->view('sb-admin/sb-admin');
+
+	}//public function index()
+
+	public function verify() 
+	{
+		echo '<h2>username : '.$this->input->post('username').'</h2>';
+		echo '<h2>password : '.$this->input->post('password').__POST['password'].'</h2>';
+		//redirect('/Student');
 	}
-}
+
+	public function show()
+	{
+		echo '$_POST'."<pre>";
+		print_r($_POST);
+		echo "</pre>";
+	}
+
+	public function test_path() 
+	{
+		echo '<h2>__FILE__ = '. __FILE__ .'</h2>';
+		echo '<h2>__DIR__ = '. __DIR__ .'</h2>';
+		echo '<h2>__FUNCTION__ = '. __FUNCTION__ .'</h2>';
+		echo '<h2>__CLASS__ = '. __CLASS__ .'</h2>';
+		echo '<h2>__TRAIT__ = '. __TRAIT__ .'</h2>';
+		echo '<h2>__METHOD__ = '. __METHOD__ .'</h2>';
+		echo '<h2>__NAMESPACE__ = '. __NAMESPACE__ .'</h2>';
+	}
+}//class Welcome
+?>
