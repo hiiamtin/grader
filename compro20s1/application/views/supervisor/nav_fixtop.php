ca1dc28921262950338730e174b3d7311ee0d5fb
Sakda Suwantheerangkoon <sakdakluy@gmail.com>
2021-02-22 15:03:27 +0700

    [supervisor] new top navigation design

--- compro20s1/application/views/supervisor/nav_fixtop.php
+++ compro20s1/application/views/supervisor/nav_fixtop.php
@@ -1,34 +1,52 @@
+<style>
+  .navbar-inverse {
+    min-height: 90px;
+  }
+  .navbar-toggle {
+    min-height: 70px;
+  }
+  #top-menu{
+    font-weight: bold;
+  }
 
-<div class="navbar navbar-fixed-top navbar-default">
-  	<div class="container-fulid" >
-			
-		<a href="<?php echo site_url()?>" class="navbar-left"><img height="80px" width="80px" src="<?php echo base_url('assets/images/logo1.png')?>" style="padding-top:5px;padding-bottom:5px;"></a>
+</style>
 
-		<div class="navbar-text" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-bottom:5px;">
-		<h3>Programming Lab Management System</h3>
-		<h5>King Mongkut's Institute of Technolygy Ladkrabang</h5>
-		</div>
-		<div class="nav navbar-nav navbar-right" style="margin-top:15px;margin-right:15px;" >
-			<li><a href="<?php echo site_url('supervisor/index'); ?>">Home</a></li>
-			<?php if (substr($_SESSION['id'],0,2) =='90') 
-					echo '<li><a href="'. site_url($_SESSION['role'].'/exercise_show').'" title="Exercise Management">Exercise</a></li>';
-			?>
-			<li><a href="<?php echo site_url($_SESSION['role'].'/group_management'); ?>" title="Group Mangement"> Group Management </a></li>
-			<li><a href="<?php echo site_url($_SESSION['role'].'/exam_room_panel'); ?>">Exam Room</a></li>		
-			<li><a href="<?php echo site_url($_SESSION['role'].'/edit_profile_form'); ?>">Edit profile</a></li>	
-			<li><a href="<?php echo site_url("auth/logout") ?>" class="btn btn-default btn-lg"> Log out. <span class="glyphicon glyphicon-log-out"></span> </a></li> 
-		</div>
-      
-        <!--/.navbar-collapse -->
+<nav class="navbar navbar-inverse" >
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo site_url('supervisor/index'); ?>">
+        <img height="60px" width="60px" alt="Brand" src="<?php echo base_url('assets/images/logo1.png')?>">
+      </a>
     </div>
-</div>
-<div class="container-fluid"  style="background-color:HoneyDew;">	
-	<div class="panel panel-default">
-		<div class="panel-body"></div>
-	</div>
-</div>
-<div class="clear-fix"></div> 
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+      <ul class="nav navbar-nav" id="top-menu">
+        <li class="active" id="home-btn">
+          <a href="<?php echo site_url('supervisor/index'); ?>">Programming Lab<br>Management System<br>KMITL<span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li><br><a href="<?php echo site_url($_SESSION['role'].'/group_management'); ?>" title="Group Mangement"> Group Management </a></li>
+        <li><br><a href="<?php echo site_url($_SESSION['role'].'/exam_room_panel'); ?>">Exam Room</a></li>
+        <li><br><a href="<?php echo site_url($_SESSION['role'].'/edit_profile_form'); ?>">Edit Profile</a></li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right" id="top-menu">
+        <br>
+        <li><a href="<?php echo site_url("auth/logout") ?>">Log Out</a></li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
 <!-- Page Contents -->
-<div class="container-fluid"  style="background-color:HoneyDew ;margin-top:10px;">	
-	<!-- row content -->
-	<div class="row">
+<div class="container-fluid"  style="background-color:GhostWhite">
+  <!-- row content -->
+  <div class="row content">
